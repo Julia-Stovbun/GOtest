2f536e28838d6f18a898b634124df5aecc6456a7
Julia <j.stovbun@rapeed.ai>
2021-08-07 21:18:01 +0300

    html

--- main.go
+++ main.go
@@ -14,18 +14,20 @@ type User struct {
 	happiness  float64
 }
 
-func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and hi has money equal: %d", u.name, u.age, u.money)
-}
+// func (u *User) getAllInfo() string {
+// 	return fmt.Sprintf("User name is: %s. He is %d and hi has money equal: %d", u.name, u.age, u.money)
+// }
 
-func (u *User) setNewName(newName string) {
-	u.name = newName
-}
+// func (u *User) setNewName(newName string) {
+// 	u.name = newName
+// }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	bob := User{"Bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Alex")
-	fmt.Fprintf(w, bob.getAllInfo())
+	// bob.setNewName("Alex")
+	// fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
